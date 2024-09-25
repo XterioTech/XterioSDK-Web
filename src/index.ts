4fdb86ef974b9f2b9235ee7f5c3033c086df7f81
ChenWei <chen158_7@126.com>
2024-09-25 11:34:16 +0800

    release wallet v0.0.8-patch

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { useXterioWalletContext, XterioWalletProvider } from './contexts/index.tsx'
+import { IPnTransactionState, useXterioTransaction } from './contexts/pnTransaction.ts'
+import { Transaction, SendTransactionMode } from '@particle-network/aa'
+import { IXterioWalletContextProps } from './interfaces/types.ts'
+
+export { XterioWalletProvider, useXterioWalletContext, useXterioTransaction, SendTransactionMode }
+
+export type { IXterioWalletContextProps, IPnTransactionState, Transaction }
+export * from './interfaces/dappType.ts'
+
+import { Buffer } from 'buffer'
+if (typeof window !== 'undefined' && !window.Buffer) {
+  window.Buffer = Buffer
+}
+
+import './common/styles/main.css'
