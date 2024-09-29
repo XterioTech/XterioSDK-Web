f57d5fb78cd52f243cbbfd6b748f270ec3a687bf
ChenWei <chen158_7@126.com>
2024-09-29 16:44:34 +0800

    release wallet v0.0.9

---
+++ example-wallet-react/src/main.tsx
@@ -0,0 +1,51 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+const GET_QUERY_STRING = (key: string) => {
+  const ls = location.search || location.hash
+  //var reg = eval("new RegExp('[a-zA-Z0-9]+=[^&]+&|[a-zA-Z0-9]+=[^&]+$','g')");
+  const reg = eval("new RegExp('[?&]+(" + key + ')=[^&]+&|[?&]+(' + key + ")=[^&]+$')")
+  const args = ls.match(reg)
+  if (args) {
+    return args[0].split('=')[1].replace(/&$/, '')
+  } else {
+    //console.log(key + "不存在");
+    return null
+  }
+  //var args = ls.match(/[a-zA-Z0-9]+=[^&]+&|[a-zA-Z0-9]+=[^&]+$/g);
+}
+
+import { XterioWalletProvider } from '@xterio-sdk/wallet'
+import { Env } from '@xterio-sdk/auth'
+import '@xterio-sdk/wallet/style/main.css'
+import '@xterio-sdk/auth/style/main.css'
+
+const devConfig = {
+  redirect_uri: 'http://localhost:3000/',
+  client_id: '4gsmgur6gkp8u9ps8dlco3k7eo',
+  client_secret: 'ABC23',
+  app_id: 'apiautotest',
+  pn_app_id: '40ad8524-f844-496d-8de2-50a8a322d6ba'
+}
+const stageConfig = {
+  redirect_uri: location.href.replace(/[?&]code=[^&]+/, ''),
+  client_id: '3094298453404953',
+  client_secret: 'mzmhYqcqDGdymblv5gb7s9OWcnYpH1ha',
+  app_id: '6c684e202700',
+  pn_app_id: '9cd6a325-3082-4e98-8803-82a66cd9e86f'
+}
+let _env: Env = __EXAMPLE_ENV__ || Env.Dev
+const variable = GET_QUERY_STRING('env')
+if (variable === 'dev') {
+  _env = Env.Dev
+} else if (variable === 'stage') {
+  _env = Env.Staging
+}
+const config = _env === Env.Staging ? stageConfig : devConfig
+createRoot(document.getElementById('root')!).render(
+  // <StrictMode>
+  <XterioWalletProvider {...config} env={_env} showOpenWalletIcon enableAuthInit>
+    <App />
+  </XterioWalletProvider>
+  // </StrictMode>
+)
---
+++ src/App.tsx
@@ -0,0 +1,164 @@
+import './App.css'
+import { useXterioTransaction, useXterioWalletContext } from './index'
+import { useState } from 'react'
+import { Contract, ContractInterface, ethers } from 'ethers'
+import { ERC20_ABI } from './common/abi'
+import { LoginType } from '@xterio-sdk/auth'
+
+/**
+ * 区分区块链网络的名称，平台前后端统一定义的枚举类型
+ * 注意：请勿和ethers、web3等第三方库中的network定义混淆
+ */
+export enum NETWORK_NAME {
+  ETHEREUM = 'ETHEREUM',
+  BSC = 'BSC',
+  OPBNB = 'OPBNB',
+  ARBITRUM = 'ARBITRUM',
+  POLYGON = 'POLYGON',
+  XTERIO = 'XTERIO',
+  XTERIO_ETH = 'XTERIO_ETH',
+  BASE = 'BASE',
+
+  GOERLI = 'GOERLI',
+  SEPOLIA = 'SEPOLIA',
+  BSC_TESTNET = 'BSC_TESTNET',
+  OPBNB_TESTNET = 'OPBNB_TESTNET',
+  XTERIO_TESTNET = 'XTERIO_TESTNET'
+}
+
+export const getJsonRPCUrl = (network?: string) => {
+  switch (network) {
+    case NETWORK_NAME.ETHEREUM:
+      return `https://ethereum.publicnode.com`
+    case NETWORK_NAME.ARBITRUM:
+      return `https://arbitrum-one.publicnode.com`
+    case NETWORK_NAME.POLYGON:
+      return `https://polygon-bor.publicnode.com`
+    case NETWORK_NAME.OPBNB:
+      return `https://opbnb-mainnet-rpc.bnbchain.org`
+    case NETWORK_NAME.BSC:
+      return 'https://bsc-dataseed.bnbchain.org'
+    case NETWORK_NAME.XTERIO:
+      return `https://xterio-bnb.alt.technology`
+    case NETWORK_NAME.XTERIO_ETH:
+      return `https://xterio-eth.alt.technology`
+    case NETWORK_NAME.BASE:
+      return `https://mainnet.base.org`
+    case NETWORK_NAME.BSC_TESTNET:
+      return 'https://bsc-testnet-rpc.publicnode.com'
+    // return 'https://data-seed-prebsc-2-s1.binance.org:8545'
+    case NETWORK_NAME.GOERLI:
+      return 'https://eth-goerli.public.blastapi.io'
+    case NETWORK_NAME.SEPOLIA:
+      return `https://ethereum-sepolia-rpc.publicnode.com`
+    case NETWORK_NAME.OPBNB_TESTNET:
+      return `https://opbnb-testnet-rpc.bnbchain.org`
+    case NETWORK_NAME.XTERIO_TESTNET:
+      return `https://xterio-testnet.alt.technology/`
+    default:
+      throw new Error('unsupported network: ' + network)
+  }
+}
+
+export const getContract = (network: NETWORK_NAME, contractAddress: string, abi: ContractInterface) => {
+  const provider = new ethers.providers.JsonRpcProvider(getJsonRPCUrl(network))
+  // const contractAddress = ''
+  // const abi = ''
+  const contract = new Contract(contractAddress, abi, provider)
+  return contract
+}
+
+function App() {
+  const {
+    userinfo,
+    isLogin,
+    login,
+    logout,
+    aaAddress,
+    isConnect,
+    disconnectWallet,
+    openWallet,
+    obtainWallet,
+    connectWallet,
+    signMessage
+  } = useXterioWalletContext()
+
+  const contractAddress = '0x12065F0d03cd1Bd280565069164F9E803c2DA988'
+  const abi = ERC20_ABI
+  const erc20 = getContract(NETWORK_NAME.SEPOLIA, contractAddress, abi)
+  const { sendTransaction, sendUserOperation, state } = useXterioTransaction(erc20, 'transfer')
+
+  const [signedMsg, setSignedMsg] = useState('')
+
+  const test1 = async () => {
+    //方式1: sendTransaction，useXterioTransaction 必须传contract跟functionName
+    const toAddr = '0xF4Ae736B14a7B5FDb803172B242074D6DFe655bb'
+    const amount = '0x0de0b6b3a7640000'
+    try {
+      await sendTransaction?.({ gasLimit: '0x90de' }, toAddr, amount)
+    } catch (err) {
+      console.log('ddd', err)
+    }
+  }
+
+  const test2 = async () => {
+    //方式2: sendUserOperation
+    const contractAddress = '0x12065F0d03cd1Bd280565069164F9E803c2DA988'
+    const abi = ERC20_ABI
+    const erc20 = getContract(NETWORK_NAME.SEPOLIA, contractAddress, abi)
+    const toAddr = '0xF4Ae736B14a7B5FDb803172B242074D6DFe655bb'
+    const amount = '0x0de0b6b3a7640000'
+
+    const tx = {
+      to: contractAddress,
+      data: erc20.interface.encodeFunctionData('transfer', [toAddr, amount])
+    }
+    try {
+      await sendUserOperation?.(tx)
+    } catch (err) {
+      console.log('ddd', err)
+    }
+  }
+
+  return (
+    <>
+      <h1>Xterio SDK</h1>
+      <div>xterio auth sdk</div>
+      <div className="card">
+        <p>是否登录: {isLogin ? 'true' : 'false'}</p>
+        <p>用户信息: {userinfo ? JSON.stringify(userinfo) : ''}</p>
+        <button onClick={() => login()}>默认登录</button>
+        <button onClick={() => login(LoginType.Email)}>邮箱登录</button>
+        <button onClick={() => login(LoginType.Mini)}>TG 登录</button>
+        <button onClick={logout}>退出登录</button>
+      </div>
+
+      <div>xterio wallet sdk</div>
+      <div className="card">
+        <div>pn aa wallet address: {aaAddress}</div>
+        <div>pn aa wallet connected status: {isConnect ? 'true' : 'false'}</div>
+        <button onClick={() => connectWallet()}>AA钱包连接</button>
+        <button onClick={() => disconnectWallet()}>AA钱包断开连接</button>
+        <button onClick={() => obtainWallet()}>AA钱包领取</button>
+        <button onClick={() => openWallet()}>打开AA钱包</button>
+      </div>
+      <div>xterio wallet transaction</div>
+      <div className="card">
+        <div>sign message result: {signedMsg}</div>
+        <button
+          onClick={async () => {
+            const a = await signMessage?.('hello world')
+            setSignedMsg(a)
+          }}
+        >
+          签名message
+        </button>
+        <div>转账进度：{state.status}</div>
+        <button onClick={test1}>转账</button>
+        <button onClick={test2}>转账2</button>
+      </div>
+    </>
+  )
+}
+
+export default App
