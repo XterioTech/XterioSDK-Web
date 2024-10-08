1b0a38f65ebdba9acbab9cb6c8ef4e487191d707
XterioTech <XterioTech@users.noreply.github.com>
2024-10-08 10:03:08 +0000

    auto deploy example 05e92f236a1ced06202e552cafaa07d45ae98420

---
+++ wallet-react/assets/index-CKMy3IRk.js
@@ -0,0 +1,1 @@
+import{r as a,aF as f}from"./index-DlRtH56e.js";import{u as c}from"./index-CF21X8L-.js";var l=function(n,r,e){e===void 0&&(e={});var u=c(n),t=a.useRef(null),i=a.useCallback(function(){t.current&&clearInterval(t.current)},[]);return a.useEffect(function(){if(!(!f(r)||r<0))return e.immediate&&u(),t.current=setInterval(u,r),i},[r,e.immediate]),i};export{l as u};
