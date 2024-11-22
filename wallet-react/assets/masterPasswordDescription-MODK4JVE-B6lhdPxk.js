f984433d427635342473713bcde7a3002e671563
XterioTech <XterioTech@users.noreply.github.com>
2024-11-22 09:57:17 +0000

    auto deploy example 0ecdf1ca6e6316da06951cde4d5c8bf40793a4be

---
+++ wallet-react/assets/masterPasswordDescription-MODK4JVE-B6lhdPxk.js
@@ -0,0 +1,22 @@
+import{w as c}from"./chunk-IBLFF4W2-BUeVAK2W.js";import{p as n}from"./chunk-3WXPHVZ4-DxhDs8Ir.js";import{N as s}from"./chunk-6X7TBCV3-BhWC7cKW.js";import{J as m,o as e}from"./index-CUuT5o1Q.js";import{u as l}from"./useTranslation-qVTXi8Sc.js";import{C as i,T as p}from"./index-Y-offad7.js";import"./TextArea-_3ieV8lN.js";import"./colors-BpiQrCcB.js";import"./index-Dgjw1loZ.js";var u=`.master-password-description-container {
+  display: flex;
+  flex-direction: column;
+}
+.about-mp-container {
+  padding: 0 18px;
+}
+.about-mp-container .space-container {
+  width: 100%;
+  padding: 20px 0;
+}
+.about-mp-container .space-container .master-title {
+  text-align: center;
+}
+.about-mp-container .space-container .ant-typography {
+  color: var(--text-color) !important;
+}
+.about-mp-container .space-container .content {
+  font-size: 14px;
+  color: var(--secondary-text-color) !important;
+}
+`,d=()=>{let{Title:a,Paragraph:o}=p,r=m(),{t}=l();return e.createElement("div",{className:"master-password-description-container height-100"},e.createElement(c,null,t("account.about_master_password")),e.createElement("div",{className:"scroll-content"},e.createElement("style",null,u),e.createElement("div",{className:"about-mp-container"},e.createElement(n,{className:"icon-navigation-back",name:"circle_back",onClick:()=>r(-1)}),e.createElement(i,{direction:"vertical",className:"space-container"},e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t1")),e.createElement(o,{className:"content"},t("account.about_master_password_p1")),e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t2")),e.createElement(o,{className:"content"},t("account.about_master_password_p2")),e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t3")),e.createElement(o,{className:"content"},t("account.about_master_password_p3"))))),e.createElement(s,{className:"footer-box-v2"}))},y=d;export{y as default};
